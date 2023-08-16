528c9e09876194dd962e454ac9b7fba4d55a54d0
TingTing <ting@rytass.com>
2023-08-16 16:41:50 +0800

    feat: use mzn palette

--- apps/client/components/AddArea/index.tsx
+++ apps/client/components/AddArea/index.tsx
@@ -1,12 +1,20 @@
+import dynamic from 'next/dynamic';
 import classes from './index.module.scss';
+
+const AddModal = dynamic(() => import('@components/AddModal'), {
+  ssr: false,
+});
 
 const AddArea = () => {
   return (
-    <div className={classes.root}>
-      <div className={classes.area}>
-        Area
+    <>
+      <div className={classes.root}>
+        <div className={classes.area}>
+          Area
+        </div>
       </div>
-    </div>
+      <AddModal />
+    </>
   );
 }
 
---
+++ apps/client/components/AddModal/index.tsx
@@ -0,0 +1,13 @@
+import { Modal, ModalHeader, ModalBody } from '@mezzanine-ui/react';
+import classes from './index.module.scss';
+
+const AddModal = () => {
+  return (
+    <Modal>
+      <ModalHeader>HHH</ModalHeader>
+      <ModalBody>123</ModalBody>
+    </Modal>
+  );
+}
+
+export default AddModal;
