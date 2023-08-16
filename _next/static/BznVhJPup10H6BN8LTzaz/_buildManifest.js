35957d53366cbb3c9b9cd8e275c736d697139574
TingTing <ting@rytass.com>
2023-08-17 02:49:43 +0800

    Updates

---
+++ _next/static/BznVhJPup10H6BN8LTzaz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cca5b8ec9cd0640f.css","static/chunks/pages/index-75a4266daf356ffc.js"],"/_error":["static/chunks/pages/_error-d11cb7facb2c59a4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
