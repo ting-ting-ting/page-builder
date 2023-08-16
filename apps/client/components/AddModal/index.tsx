d629a4069dda35b1467a73661fcf80b96381f2d3
TingTing <ting@rytass.com>
2023-08-16 17:49:43 +0800

    feat: use react hook form

--- apps/client/components/AddModal/index.tsx
+++ apps/client/components/AddModal/index.tsx
@@ -1,5 +1,8 @@
-import { Modal, ModalHeader, ModalBody, Button } from '@mezzanine-ui/react';
+import { Modal, ModalHeader, Button } from '@mezzanine-ui/react';
+import { useForm } from 'react-hook-form';
 import Paragraph1 from '@components/Templates/Paragraph/Paragraph1';
+import FormFieldsWrapper from '@components/Form/FormFieldsWrapper';
+import TextAreaField from '@components/Form/TextAreaField';
 import classes from './index.module.scss';
 
 type AddModalProps = {
@@ -11,6 +14,12 @@ const AddModal = ({
   open,
   onClose,
 } : AddModalProps) => {
+  const methods = useForm<{
+    p1: string;
+    p2: string;
+    p3: string;
+  }>();
+
   return (
     <Modal
       open={open}
@@ -18,16 +27,29 @@ const AddModal = ({
       className={classes.root}
       direction="left"
     >
-      <ModalHeader className={classes.modalHeader}>選擇模板</ModalHeader>
-      <div className={classes.modalBody}>
-        <div className={classes.previewWrapper}>
-          <Paragraph1 />
+      <FormFieldsWrapper
+        methods={methods}
+        className={classes.form}
+        onSubmit={data => {
+          console.log('data', data);
+        }}
+      >
+        <ModalHeader className={classes.modalHeader}>選擇模板</ModalHeader>
+        <div className={classes.modalBody}>
+          <div className={classes.previewWrapper}>
+            <Paragraph1 />
+          </div>
+          <div className={classes.fieldsWrapper}>
+            <TextAreaField label="段落一" registerName="p1" />
+            <TextAreaField label="段落二" registerName="p2" />
+            <TextAreaField label="段落三" registerName="p3" />
+          </div>
         </div>
-      </div>
-      <div className={classes.modalFooter}>
-        <Button type="button" variant="contained" size="large">關閉</Button>
-        <Button type="button" variant="contained" size="large">確定</Button>
-      </div>
+        <div className={classes.modalFooter}>
+          <Button type="button" variant="contained" size="large">關閉</Button>
+          <Button type="submit" variant="contained" size="large">確定</Button>
+        </div>
+      </FormFieldsWrapper>
     </Modal>
   );
 }
