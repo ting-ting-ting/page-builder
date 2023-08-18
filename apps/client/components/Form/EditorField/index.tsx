2bac8dba320abe57214cdae6afa82f9bcb1a7b05
TingTing <ting@rytass.com>
2023-08-18 16:28:51 +0800

    feat: editor field

--- apps/client/components/Form/EditorField/index.tsx
+++ apps/client/components/Form/EditorField/index.tsx
@@ -1,24 +1,59 @@
-import { useState } from 'react';
+import { useMemo } from 'react';
 import Quill from 'react-quill';
+import { useFormContext, useFormState, useWatch } from 'react-hook-form';
+import { RegisteredFieldProps, HookFormFieldType } from '../typing';
 import 'quill/dist/quill.snow.css';
+import classes from './index.module.scss';
 
-const EditorField = () => {
-  const [editorState, setEditorState] = useState('');
+export type EditorFieldProps = RegisteredFieldProps<HookFormFieldType> & {
+  label?: string;
+  defaultValue?: string;
+};
 
-  console.log('gggg', editorState)
+const EditorField = ({
+  control,
+  label,
+  defaultValue,
+  register,
+  registerName,
+} : EditorFieldProps) => {
+  const {
+    control: contextControl,
+    setValue,
+    register: contextRegister,
+  } = useFormContext();
+
+  const watchValue =
+    useWatch({
+      control: control || contextControl,
+      name: registerName as string,
+      defaultValue,
+    }) || "";
+
+  const { errors } = useFormState({ control: control || contextControl });
+
+  const registration = useMemo(
+    () =>
+      (register || contextRegister)(
+        registerName,
+      ),
+    [register, contextRegister, registerName]
+  );
 
   return (
-    <Quill
-      onChange={v => {
-        setEditorState(v);
-      }}
-      theme="snow"
-      modules={{
-        toolbar: [['bold', 'italic', 'underline', 'link']],
-      }}
-      value={editorState}
-    />
+    <div className={classes.root}>
+      {label && (
+        <p className={classes.label}>{label}</p>
+      )}
+      <Quill
+        className={classes.editor}
+        onChange={value => {
+          setValue(registerName, value);
+        }}
+        value={watchValue}
+      />
+    </div>
   );
-}
+};
 
 export default EditorField;
