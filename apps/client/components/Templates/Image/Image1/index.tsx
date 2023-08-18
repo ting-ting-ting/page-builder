69cbb167381b661baf3b3fa5d7a89df0eeee49cc
TingTing <ting@rytass.com>
2023-08-19 01:48:40 +0800

    refactor: typing

--- apps/client/components/Templates/Image/Image1/index.tsx
+++ apps/client/components/Templates/Image/Image1/index.tsx
@@ -3,14 +3,11 @@ import Template from './Template';
 import Preview from './Preview';
 import { ImageIdEnum, IMAGE_CATEGORY } from '../share';
 import { Image1Type } from './typing';
+import { ImageBaseDataType } from '../typing';
 
 export const image1Index: {
-  [ImageIdEnum.IMAGE_1]: {
-    id: ImageIdEnum.IMAGE_1;
-    category: typeof IMAGE_CATEGORY;
-    Editor: () => JSX.Element;
+  [ImageIdEnum.IMAGE_1]: ImageBaseDataType & {
     Template: (props: Image1Type) =>  JSX.Element;
-    Preview: () => JSX.Element;
     defaultValues: Image1Type;
   };
 } = {
--- apps/client/components/Templates/Image/typing.d.ts
+++ apps/client/components/Templates/Image/typing.d.ts
@@ -1,3 +1,11 @@
 import { Image1Type } from './Image1/typing';
+import { ImageIdEnum, IMAGE_CATEGORY } from './share';
 
 export type ImageTypes = Image1Type;
+
+export type ImageBaseDataType = {
+  id: ImageIdEnum;
+  category: typeof IMAGE_CATEGORY;
+  Editor: () => JSX.Element;
+  Preview: () => JSX.Element;
+}
