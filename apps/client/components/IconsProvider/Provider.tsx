ae47dcc85c2bf1f3f4f8c0182652a84019875327
TingTing <ting@rytass.com>
2023-08-22 13:44:52 +0800

    feat: icons base style

--- apps/client/components/IconsProvider/Provider.tsx
+++ apps/client/components/IconsProvider/Provider.tsx
@@ -1,7 +1,9 @@
 import { useCallback, useMemo, ReactNode } from 'react';
+import cx from 'classnames';
 import { IconsContextProvider } from './Context';
 import { ReactComponent as FacebookIcon } from './icons/facebook.svg';
 import { ReactComponent as GithubIcon } from './icons/github.svg';
+import classes from './index.module.scss';
 
 export const FACEBOOK_ICON = 'FACEBOOK_ICON';
 export const GITHUB_ICON = 'GITHUB_ICON';
@@ -10,10 +12,10 @@ const IconsProvider = ({ children } : { children: ReactNode }) => {
   const getIcon = useCallback((iconId: string, className?: string) => {
     switch (iconId) {
       case FACEBOOK_ICON:
-        return <FacebookIcon className={className} />;
+        return <FacebookIcon className={cx(classes.icon, className)} />;
 
       case GITHUB_ICON:
-        return <GithubIcon className={className} />;
+        return <GithubIcon className={cx(classes.icon, className)} />;
 
       default:
         return null;
