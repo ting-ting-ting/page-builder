b59e247879a5ff4a6e6a07b9240e4193fb646f03
TingTing <ting@rytass.com>
2023-08-22 13:54:01 +0800

    feat: create icon field component

---
+++ apps/client/components/Form/IconField/index.tsx
@@ -0,0 +1,71 @@
+import { useMemo } from 'react';
+import { Input, InputProps } from '@mezzanine-ui/react';
+import { useFormContext, useWatch } from 'react-hook-form';
+import { RegisteredFieldProps, HookFormFieldType } from '../typing';
+import classes from './index.module.scss';
+
+export type IconFieldProps = InputProps & RegisteredFieldProps<HookFormFieldType> & {
+  label?: string;
+};
+
+const IconField = ({
+  clearable,
+  control,
+  defaultValue,
+  disabled,
+  label,
+  placeholder = "請輸入",
+  register,
+  registerName,
+  required,
+  style,
+} : IconFieldProps) => {
+  const {
+    control: contextControl,
+    register: contextRegister,
+  } = useFormContext();
+
+  const watchValue =
+    useWatch({
+      control: control || contextControl,
+      name: registerName as string,
+      defaultValue,
+    }) || "";
+
+  const registration = useMemo(
+    () =>
+      (register || contextRegister)(
+        registerName,
+        {
+          required,
+          disabled,
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      ),
+    [register, contextRegister, registerName, required, disabled]
+  );
+
+  return (
+    <div className={classes.root}>
+      {label && (
+        <p className={classes.label}>{label}</p>
+      )}
+      <Input
+        fullWidth
+        clearable={clearable}
+        className={classes.input}
+        inputProps={{
+          id: registerName,
+          name: registerName,
+          onBlur: registration.onBlur,
+        }}
+        {...registration}
+        onChange={registration.onChange}
+        placeholder={placeholder}
+        value={watchValue}
+      />
+    </div>
+  );
+};
+
+export default IconField;
