3877a89371edd35a29007cd7cb6433230df9722d
TingTing <ting@rytass.com>
2023-08-22 14:59:57 +0800

    feat: icon field

--- apps/client/components/Form/IconField/index.tsx
+++ apps/client/components/Form/IconField/index.tsx
@@ -1,27 +1,18 @@
-import { useMemo, useState, useRef } from 'react';
-import { Input, InputProps, useClickAway } from '@mezzanine-ui/react';
+import { useState, useRef } from 'react';
+import { useClickAway } from '@mezzanine-ui/react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import Menu from '@components/Menu';
 import { useIcons } from '@components/IconsProvider/useIcons';
 import { RegisteredFieldProps, HookFormFieldType } from '../typing';
 import classes from './index.module.scss';
 
-export type IconFieldProps = InputProps & RegisteredFieldProps<HookFormFieldType> & {
-  label?: string;
-};
+export type IconFieldProps =  RegisteredFieldProps<HookFormFieldType>
 
 const IconField = ({
-  clearable,
   control,
-  defaultValue,
-  disabled,
-  label,
-  register,
   registerName,
-  required,
-  style,
 } : IconFieldProps) => {
-  const { icons } = useIcons();
+  const { icons, getIcon } = useIcons();
   const [open, setOpen] = useState<boolean>(false);
   const nodeRef = useRef<HTMLDivElement>(null);
   const controlRef = useRef<HTMLDivElement>(null);
@@ -44,34 +35,17 @@ const IconField = ({
 
   const {
     control: contextControl,
-    register: contextRegister,
+    setValue,
   } = useFormContext();
 
   const watchValue =
     useWatch({
       control: control || contextControl,
       name: registerName as string,
-      defaultValue,
-    }) || "";
-
-  const registration = useMemo(
-    () =>
-      (register || contextRegister)(
-        registerName,
-        {
-          required,
-          disabled,
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      ),
-    [register, contextRegister, registerName, required, disabled]
-  );
+    }) || '';
 
   return (
     <div className={classes.root}>
-      {label && (
-        <p className={classes.label}>{label}</p>
-      )}
       <div className={classes.fieldsWrapper}>
         <div ref={nodeRef}>
           <div
@@ -81,7 +55,13 @@ const IconField = ({
               setOpen(s => !s);
             }}
           >
-            <span className={classes.iconPlaceholder}>Icon</span>
+            {watchValue ? (
+              <div className={classes.iconValue}>
+                {getIcon(watchValue)}
+              </div>
+            ) : (
+              <span className={classes.iconPlaceholder}>Icon</span>
+            )}
           </div>
           <Menu
             disablePortal
@@ -90,28 +70,19 @@ const IconField = ({
           >
             <div className={classes.iconsWrapper}>
               {icons.map(icon => (
-                <div key={icon.id} className={classes.iconBtn}>
+                <div
+                  key={icon.id}
+                  className={classes.iconBtn}
+                  onClick={() => {
+                    setValue(registerName, icon.id);
+                  }}
+                >
                   {icon.icon}
                 </div>
               ))}
             </div>
           </Menu>
         </div>
-        <Input
-          fullWidth
-          clearable={clearable}
-          className={classes.input}
-          inputProps={{
-            id: registerName,
-            name: registerName,
-            onBlur: registration.onBlur,
-          }}
-          {...registration}
-          onChange={registration.onChange}
-          placeholder="請輸入網址"
-          size="large"
-          value={watchValue}
-        />
       </div>
     </div>
   );
