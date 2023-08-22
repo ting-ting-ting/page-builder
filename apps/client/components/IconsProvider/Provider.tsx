fd4e65f05a8d956f377a9d7c27cedd0a1c5e3810
TingTing <ting@rytass.com>
2023-08-22 21:24:02 +0800

    feat: add icons

--- apps/client/components/IconsProvider/Provider.tsx
+++ apps/client/components/IconsProvider/Provider.tsx
@@ -3,10 +3,22 @@ import cx from 'classnames';
 import { IconsContextProvider } from './Context';
 import { ReactComponent as FacebookIcon } from './icons/facebook.svg';
 import { ReactComponent as GithubIcon } from './icons/github.svg';
+import { ReactComponent as InstagramIcon } from './icons/instagram.svg';
+import { ReactComponent as LinkedinIcon } from './icons/linkedin.svg';
+import { ReactComponent as MediumIcon } from './icons/medium.svg';
+import { ReactComponent as TiktokIcon } from './icons/tiktok.svg';
+import { ReactComponent as WebsiteIcon } from './icons/website.svg';
+import { ReactComponent as YoutubeIcon } from './icons/youtube.svg';
 import classes from './index.module.scss';
 
-export const FACEBOOK_ICON = 'FACEBOOK_ICON';
-export const GITHUB_ICON = 'GITHUB_ICON';
+const FACEBOOK_ICON = 'FACEBOOK_ICON';
+const GITHUB_ICON = 'GITHUB_ICON';
+const INSTAGRAM_ICON = 'INSTAGRAM_ICON';
+const LINKEDIN_ICON = 'LINKEDIN_ICON';
+const MEDIUM_ICON = 'MEDIUM_ICON';
+const TIKTOK_ICON = 'TIKTOK_ICON';
+const WEBSITE_ICON = 'WEBSITE_ICON';
+const YOUTUBE_ICON = 'YOUTUBE_ICON';
 
 const IconsProvider = ({ children } : { children: ReactNode }) => {
   const getIcon = useCallback((iconId: string, className?: string) => {
@@ -17,12 +29,39 @@ const IconsProvider = ({ children } : { children: ReactNode }) => {
       case GITHUB_ICON:
         return <GithubIcon className={cx(classes.icon, className)} />;
 
+      case INSTAGRAM_ICON:
+        return <InstagramIcon className={cx(classes.icon, className)} />;
+
+      case LINKEDIN_ICON:
+        return <LinkedinIcon className={cx(classes.icon, className)} />;
+
+      case MEDIUM_ICON:
+        return <MediumIcon className={cx(classes.icon, className)} />;
+
+      case TIKTOK_ICON:
+        return <TiktokIcon className={cx(classes.icon, className)} />;
+
+      case WEBSITE_ICON:
+        return <WebsiteIcon className={cx(classes.icon, className)} />;
+
+      case YOUTUBE_ICON:
+        return <YoutubeIcon className={cx(classes.icon, className)} />;
+
       default:
         return null;
     }
   }, []);
 
-  const icons = useMemo(() => [FACEBOOK_ICON, GITHUB_ICON].map(iconId => ({
+  const icons = useMemo(() => [
+    FACEBOOK_ICON,
+    GITHUB_ICON,
+    INSTAGRAM_ICON,
+    LINKEDIN_ICON,
+    MEDIUM_ICON,
+    TIKTOK_ICON,
+    WEBSITE_ICON,
+    YOUTUBE_ICON,
+  ].map(iconId => ({
     id: iconId,
     icon: getIcon(iconId),
   })), [getIcon]);
