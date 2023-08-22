69fd495c9580992ea0f1905aa226a5b4933493cd
TingTing <ting@rytass.com>
2023-08-22 17:43:42 +0800

    feat: add profile schema

--- apps/client/components/Templates/Profiles/Profiles1/index.tsx
+++ apps/client/components/Templates/Profiles/Profiles1/index.tsx
@@ -1,14 +1,17 @@
+import * as Yup from 'yup';
 import Editor from './Editor';
 import Template from './Template';
 import Preview from './Preview';
 import { ProfilesIdEnum, PROFILES_CATEGORY } from '../share';
 import { Profiles1Type } from './typing';
+import { schema } from './schema';
 import { ProfilesBaseDataType } from '../typing';
 
 export const profiles1Index: {
   [ProfilesIdEnum.PROFILES_1]: ProfilesBaseDataType & {
     Template: (props: Profiles1Type) =>  JSX.Element;
     defaultValues: Profiles1Type;
+    schema: Yup.ObjectSchema<Profiles1Type>;
   };
 } = {
   [ProfilesIdEnum.PROFILES_1]: {
@@ -17,6 +20,7 @@ export const profiles1Index: {
     Editor: Editor,
     Template: Template,
     Preview: Preview,
+    schema,
     defaultValues: {
       profiles1_avatar: '',
       profiles1_desc: '<p>我是簡介</p>',
